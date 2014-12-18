34b44e7a41f8b6db4d16a9adaae0e18033deebee
David Hardy <david.hardy@philips.com>
2014-12-18 23:49:43 +0100

    Trimmed activation code and username

--- Application/KBF/src/nl/endran/showcase/application/kbf/LoginActivityView.java
+++ Application/KBF/src/nl/endran/showcase/application/kbf/LoginActivityView.java
@@ -16,103 +16,103 @@ import android.widget.Toast;
 
 public class LoginActivityView {
 
-	public interface LoginActivityViewListener {
+    public interface LoginActivityViewListener {
 
-		public void onOkClicked();
+        public void onOkClicked();
 
-		public void onLogoutClicked();
-	}
+        public void onLogoutClicked();
+    }
 
-	private final Activity activity;
+    private final Activity activity;
 
-	private final Button buttonOK;
-	private final Button buttonLogout;
-	private final EditText editTextActivationCode;
-	private final EditText editTextUserName;
-	private final Spinner spinnerEmail;
+    private final Button buttonOK;
+    private final Button buttonLogout;
+    private final EditText editTextActivationCode;
+    private final EditText editTextUserName;
+    private final Spinner spinnerEmail;
 
-	@SuppressLint("NewApi")
-	public LoginActivityView(final Activity activity, final LoginActivityViewListener listener) {
+    @SuppressLint("NewApi")
+    public LoginActivityView(final Activity activity, final LoginActivityViewListener listener) {
 
-		this.activity = activity;
-		activity.setContentView(R.layout.activity_login);
+        this.activity = activity;
+        activity.setContentView(R.layout.activity_login);
 
-		buttonOK = (Button) activity.findViewById(R.id.buttonOK);
-		buttonOK.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(final View view) {
-				listener.onOkClicked();
-			}
-		});
+        buttonOK = (Button) activity.findViewById(R.id.buttonOK);
+        buttonOK.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                listener.onOkClicked();
+            }
+        });
 
-		buttonLogout = (Button) activity.findViewById(R.id.buttonLogout);
-		buttonLogout.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(final View view) {
-				listener.onLogoutClicked();
-			}
-		});
+        buttonLogout = (Button) activity.findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                listener.onLogoutClicked();
+            }
+        });
 
-		editTextActivationCode = (EditText) activity.findViewById(R.id.editTextActivationCode);
+        editTextActivationCode = (EditText) activity.findViewById(R.id.editTextActivationCode);
 
-		editTextUserName = (EditText) activity.findViewById(R.id.editTextUserName);
+        editTextUserName = (EditText) activity.findViewById(R.id.editTextUserName);
 
-		spinnerEmail = (Spinner) activity.findViewById(R.id.spinnerEmail);
-		final ArrayAdapter<String> spinnerCoordinatesArrayAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item,
-				new ArrayList<String>());
-		spinnerCoordinatesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinnerEmail.setAdapter(spinnerCoordinatesArrayAdapter);
+        spinnerEmail = (Spinner) activity.findViewById(R.id.spinnerEmail);
+        final ArrayAdapter<String> spinnerCoordinatesArrayAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item,
+                new ArrayList<String>());
+        spinnerCoordinatesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEmail.setAdapter(spinnerCoordinatesArrayAdapter);
 
-		buttonOK.setEnabled(!ApplicationState.isLoggedIn());
-		editTextUserName.setEnabled(!ApplicationState.isLoggedIn());
-		spinnerEmail.setEnabled(!ApplicationState.isLoggedIn());
+        buttonOK.setEnabled(!ApplicationState.isLoggedIn());
+        editTextUserName.setEnabled(!ApplicationState.isLoggedIn());
+        spinnerEmail.setEnabled(!ApplicationState.isLoggedIn());
 
-		if (ApplicationState.isActivated()) {
-			editTextActivationCode.setEnabled(false);
-			editTextActivationCode.setText(R.string.activationCode);
-			buttonLogout.setVisibility(View.VISIBLE);
-		} else {
-			editTextActivationCode.setEnabled(true);
-			buttonLogout.setVisibility(View.GONE);
-		}
-	}
+        if (ApplicationState.isActivated()) {
+            editTextActivationCode.setEnabled(false);
+            editTextActivationCode.setText(R.string.activationCode);
+            buttonLogout.setVisibility(View.VISIBLE);
+        } else {
+            editTextActivationCode.setEnabled(true);
+            buttonLogout.setVisibility(View.GONE);
+        }
+    }
 
-	public void showError(final int messageId) {
-		showError(activity.getString(messageId));
-	}
+    public void showError(final int messageId) {
+        showError(activity.getString(messageId));
+    }
 
-	public void showError(final String message) {
-		Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
-	}
+    public void showError(final String message) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+    }
 
-	public void loggedIn(final String userName) {
-		editTextUserName.setText(userName);
-		buttonLogout.setVisibility(View.VISIBLE);
-	}
+    public void loggedIn(final String userName) {
+        editTextUserName.setText(userName);
+        buttonLogout.setVisibility(View.VISIBLE);
+    }
 
-	public void setEmailList(final List<String> emailList) {
-		final ArrayAdapter<String> spinnerCoordinatesArrayAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item, emailList);
-		spinnerCoordinatesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinnerEmail.setAdapter(spinnerCoordinatesArrayAdapter);
-	}
+    public void setEmailList(final List<String> emailList) {
+        final ArrayAdapter<String> spinnerCoordinatesArrayAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item, emailList);
+        spinnerCoordinatesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEmail.setAdapter(spinnerCoordinatesArrayAdapter);
+    }
 
-	public String getActivationCode() {
-		return editTextActivationCode.getText().toString();
-	}
+    public String getActivationCode() {
+        return editTextActivationCode.getText().toString().trim();
+    }
 
-	public String getUserName() {
-		return editTextUserName.getText().toString();
-	}
+    public String getUserName() {
+        return editTextUserName.getText().toString().trim();
+    }
 
-	public void showMessage(final int messageId) {
-		showMessage(activity.getString(messageId));
-	}
+    public void showMessage(final int messageId) {
+        showMessage(activity.getString(messageId));
+    }
 
-	public void showMessage(final String message) {
-		showError(message);
-	}
+    public void showMessage(final String message) {
+        showError(message);
+    }
 
-	public String getEmail() {
-		return (String) spinnerEmail.getSelectedItem();
-	}
+    public String getEmail() {
+        return (String) spinnerEmail.getSelectedItem();
+    }
 }
